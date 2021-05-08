8f5a7f6d53e0594aea14e62d55565298ac76e9fd
kahomayo <kahomayo@pm.me>
2021-05-08 17:01:48 +0200

    Render test canvas

--- monolith-renderer/src/lib.rs
+++ monolith-renderer/src/lib.rs
@@ -1,5 +1,5 @@
 use image::{Rgba, RgbaImage};
-use monolith_finder::coord::BlockPos2D;
+use monolith_finder::coord::{BlockPos2D, SamplePos2D};
 use monolith_finder::finder::search_monoliths;
 use monolith_finder::worldgen::ChunkGenerator;
 use wasm_bindgen::prelude::*;
@@ -11,6 +11,37 @@ pub const BYTES_PER_PIXEL: usize = 4;
 pub struct RenderJob {
     chunk_generator: ChunkGenerator,
     results: Vec<u8>,
+}
+
+impl RenderJob {
+    pub fn render_section_to_buf(&mut self, start_pos: BlockPos2D) {
+        let mut image = RgbaImage::new(TILE_SIZE, TILE_SIZE);
+        let start_pos: SamplePos2D = start_pos.into();
+        for fragment_x in 0..64u32 {
+            for fragment_z in 0..64u32 {
+                let pos = SamplePos2D {
+                    x: start_pos.x + (4 * fragment_x as i32),
+                    z: start_pos.z + (4 * fragment_z as i32),
+                };
+                let is_monolith = search_monoliths(&self.chunk_generator, pos.into(), 4, 4);
+                for px_x in 0..4u32 {
+                    for px_z in 0..4u32 {
+                        let is_monolith = is_monolith[(4 * px_x + px_z) as usize];
+                        assert_eq!(
+                            *image.get_pixel(fragment_x * 4 + px_x, fragment_z * 4 + px_z),
+                            Rgba([0, 0, 0, 0])
+                        );
+                        image.put_pixel(
+                            fragment_x * 4 + px_x,
+                            fragment_z * 4 + px_z,
+                            Rgba([0, if is_monolith { 255 } else { 0 }, 128, 255]),
+                        );
+                    }
+                }
+            }
+        }
+        self.results = image.into_raw();
+    }
 }
 
 #[wasm_bindgen]
@@ -26,29 +57,35 @@ impl RenderJob {
     pub fn set_seed(&mut self, seed: u64) {
         self.chunk_generator = ChunkGenerator::new(seed);
     }
-
     pub fn render_section(&mut self, block_x: i32, block_z: i32) -> *const u8 {
-        let mut image = RgbaImage::new(TILE_SIZE, TILE_SIZE);
-        for fragment_x in 0..64u32 {
-            for fragment_z in 0..64u32 {
-                let pos = BlockPos2D {
-                    x: block_x + (4 * fragment_x as i32),
-                    z: block_z + (4 * fragment_z as i32),
-                };
-                let is_monolith = search_monoliths(&self.chunk_generator, pos.into(), 4, 4);
-                for px_x in 0..4u32 {
-                    for px_z in 0..4u32 {
-                        let is_monolith = is_monolith[(px_z + 4 * px_x) as usize];
-                        image.put_pixel(
-                            fragment_x * 4 + px_x,
-                            fragment_z * 4 + px_z,
-                            Rgba([0, if is_monolith { 255 } else { 0 }, 128, 0]),
-                        );
-                    }
-                }
-            }
-        }
-        self.results = image.into_raw();
+        self.render_section_to_buf(BlockPos2D {
+            x: block_x,
+            z: block_z,
+        });
         self.results.as_ptr()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::RenderJob;
+    use monolith_finder::coord::BlockPos2D;
+
+    #[test]
+    fn output_is_reasonable() {
+        let mut x = RenderJob::new();
+        x.set_seed(8676641231682978167);
+        x.render_section_to_buf(BlockPos2D { x: -2624, z: 4343 });
+        assert_eq!(255, x.results[1]);
+    }
+
+    #[test]
+    fn output_is_reasonable_2() {
+        let mut x = RenderJob::new();
+        x.set_seed(8676641231682978167);
+        let res_ptr = x.render_section(-2624, 4343);
+        unsafe {
+            assert_eq!(255, *res_ptr.add(1));
+        }
     }
 }
--- monolith-renderer/www/index.js
+++ monolith-renderer/www/index.js
@@ -1,8 +1,32 @@
 import * as wasm from "monolith-renderer";
+import {memory} from "monolith-renderer/monolith_renderer_bg";
 
 var x = new wasm.RenderJob();
-x.set_seed(BigInt(15));
+x.set_seed("8676641231682978167");
 
 const canvas_element = document.getElementById("test-canvas");
 const canvas_context = canvas_element.getContext("2d");
 const canvasImageData = canvas_context.createImageData(256, 256);
+
+const result_buf = x.render_section(-2624 - 400, 4343 - 400);
+const wasmByteMemoryArray = new Uint8Array(memory.buffer);
+const imageDataArray = wasmByteMemoryArray.slice(result_buf, result_buf + 256 * 256 * 4);
+// for (let i = 0; i < canvasImageData.data.length; i+= 4) {
+//   // Percentage in the x direction, times 255
+//   let x = (i % 1024) / 1024 * 255;
+//   // Percentage in the y direction, times 255
+//   let y = Math.ceil(i / 1024) / 256 * 255;
+//
+//   // Modify pixel data
+//   canvasImageData.data[i + 0] = x;        // R value
+//   canvasImageData.data[i + 1] = y;        // G value
+//   canvasImageData.data[i + 2] = 255 - x;  // B value
+//   canvasImageData.data[i + 3] = 255;      // A value
+// }
+// canvasImageData.data.set(imageDataArray);
+for (let i = 0; i < canvasImageData.data.length; ++i) {
+canvasImageData.data[i] = imageDataArray[i];
+}
+console.log(imageDataArray);
+console.log(imageDataArray[1]);
+canvas_context.putImageData(canvasImageData, 0, 0);
