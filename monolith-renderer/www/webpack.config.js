056c2da14a92bc5ff418a175a651dc4f5d92679a
kahomayo <kahomayo@pm.me>
2021-05-09 21:25:35 +0200

    Wrangle webpack

--- monolith-renderer/www/webpack.config.js
+++ monolith-renderer/www/webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    // Webpack is infuriating to deal with, it seems to want thousands of
+    // plugins for something as simple as "include leaflet.css on this page". I
+    // guess just providing a list of files to copy is at least a good escape
+    // hatch.
+    new CopyWebpackPlugin(['index.html', 
+    { from: 'node_modules/leaflet/LICENSE', to: 'LICENSE-Leaflet.txt' }, 
+    { from: 'leaflet.latlng-graticule/LICENSE', to: 'LICENSE-leaflet.latlng-graticule.txt' },
+    { from: 'node_modules/leaflet/dist/leaflet.css', to: 'leaflet.css'}])
   ],
 };
